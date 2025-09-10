20fafe49d0ad666f7d54316dfca3ab1ffbd05e2a
Akshat Banzal <akshatcodez@gmail.com>
2025-09-10 17:43:44 +0000

    changed horizon logo to fcc logo, greyed button

--- src/components/HorizonCard.jsx
+++ src/components/HorizonCard.jsx
@@ -1,4 +1,4 @@
-import star from "../assets/star.svg"
+import { _numWithUnitExp } from "gsap/gsap-core"
 const HorizonCard = () => {
 
     return(
@@ -12,14 +12,14 @@ const HorizonCard = () => {
 
     <h2 className="flex items-center gap-x-3 text-4xl md:text-5xl font-schibsted font-bold bg-gradient-to-r from-[#81BD01] via-[#F3FFD8] to-[#D9FE8B] bg-clip-text text-transparent text-lime-400 mb-4">
         Horizon 2026
-        <img src = {star} alt="icon" className="w-12 h-12" />
+        <img src = "/fcc-logo-blue.png" alt="icon" className="w-12 h-12" />
     </h2>
 
     <p className="max-w-3xl font-outfit font text-Body-1-fontSize md:text-2xl text-gray-300 mb-8">
         India’s Premier Student-Led Finance and Strategy Summit
     </p>
 
-    <button className="flex items-center gap-2 bg-[#CDFE64] hover:bg-blue-500 text-[#1F2E01] font-semibold font-outfit text-Body-2-fontSize px-6 py-3 rounded-md transition duration-300">
+    <button className="flex items-center gap-2 bg-[#CDFE64] text-[#1F2E01] font-semibold font-outfit text-Body-2-fontSize px-6 py-3 rounded-md transition duration-300">
         Register
         <span className="text-[#1F2E01]">→</span>
     </button>
